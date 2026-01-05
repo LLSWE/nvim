2ef9641d6ca47ff7b71ca0bb4b93a20195a84e60
SILAS-JOB <tiilas777@gmail.com>
2026-01-05 11:46:25 -0300

    update specs

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,7 @@
 return {
-    { 'dasupradyumna/midnight.nvim', lazy = false, priority = 1000 },
-    { "ellisonleao/gruvbox.nvim", config = true, opts = ...},
-    { "catppuccin/nvim", name = "catppuccin"},
-    {"sainnhe/everforest" }
+  { "dasupradyumna/midnight.nvim", lazy = false, priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", config = true },
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "sainnhe/everforest" },
+  { "LazyVim/LazyVim", opts = { colorscheme = "midnight" } },
 }
