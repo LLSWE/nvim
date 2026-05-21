8f6f1ad10e450f7d29ada4a9435ae5ee75fa6667
LLSWE <tiilas777@gmail.com>
2026-05-21 18:35:55 -0300

    more themes + gruvbox going HARD for albanian vim

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,12 +1,13 @@
 return {
   { "dasupradyumna/midnight.nvim", lazy = false },
-  { "ellisonleao/gruvbox.nvim", config = true },
+  { "ellisonleao/gruvbox.nvim", config = true, contrast = "hard" },
   { "catppuccin/nvim", name = "catppuccin" },
   { "sainnhe/everforest" },
-  { "LazyVim/LazyVim", opts = { colorscheme = "burzum" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox" } },
   { "rebelot/kanagawa.nvim" },
   { "kepano/flexoki-neovim" },
   { "everviolet/nvim" },
   { "nyoom-engineering/nyoom.nvim" },
   { "metalelf0/black-metal-theme-neovim" },
+  { "zenbones-theme/zenbones.nvim" },
 }
