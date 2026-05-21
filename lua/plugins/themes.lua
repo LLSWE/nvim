b8d7226d99a73ea1babf5a63f987d9d6c10ff4a7
LLSWE <tiilas777@gmail.com>
2026-05-21 16:46:34 -0300

    new theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,11 +1,12 @@
 return {
-  { "dasupradyumna/midnight.nvim", lazy = false, priority = 1000 },
+  { "dasupradyumna/midnight.nvim", lazy = false },
   { "ellisonleao/gruvbox.nvim", config = true },
   { "catppuccin/nvim", name = "catppuccin" },
   { "sainnhe/everforest" },
-  { "LazyVim/LazyVim", opts = { colorscheme = "midnight" } },
+  { "LazyVim/LazyVim", opts = { colorscheme = "burzum" } },
   { "rebelot/kanagawa.nvim" },
   { "kepano/flexoki-neovim" },
   { "everviolet/nvim" },
   { "nyoom-engineering/nyoom.nvim" },
+  { "metalelf0/black-metal-theme-neovim" },
 }
